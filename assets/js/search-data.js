9209111ace5d85ed91c62c815a416f2928187394
Daksh-Daksh <49864781+Daksh-Daksh@users.noreply.github.com>
2025-07-03 09:39:08 +0000

    Deploying to gh-pages from @ Daksh-Daksh/Daksh@05a224d03fe190473c0b03d494256cda1f0f6443 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-joined-max-planck-institute-for-polymer-research",
           title: 'Joined Max Planck Institute for Polymer Research.',
+          description: "",
+          section: "News",},{id: "news-presented-talk-on-multi-resolution-cross-modality-image-registration-using-unsupervised-deep-learning-approach-in-microscopy-and-microanalysis-conference-in-minneapolis-usa",
+          title: 'Presented talk on Multi-resolution Cross-modality Image Registration Using Unsupervised Deep Learning Approach in...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
