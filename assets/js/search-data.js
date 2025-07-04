c1e314662d59788a15358d18ed716fc0e0159390
Daksh-Daksh <49864781+Daksh-Daksh@users.noreply.github.com>
2025-07-04 11:25:24 +0000

    Deploying to gh-pages from @ Daksh-Daksh/Daksh-Daksh.github.io@8997e62a24d4a86c487c13f0dd26b4a39047ce67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "For the better idea of my academic profile and work experience in the past, check the pdf.",
+          description: "For the better idea of my academic profile and work experience in the past, check the pdf version.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
